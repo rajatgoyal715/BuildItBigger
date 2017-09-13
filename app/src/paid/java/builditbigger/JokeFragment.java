5305fbb4060a145c509d3ea65805ead0bf3f9be8
Rajat Goyal <rajatgoyal715@gmail.com>
2017-09-13 16:27:50 +0530

    Project Review changes

--- app/src/paid/java/builditbigger/JokeFragment.java
+++ app/src/paid/java/builditbigger/JokeFragment.java
@@ -8,9 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
 import com.rajatgoyal.builditbigger.R;
 
 /**
